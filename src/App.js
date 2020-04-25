e9512d213214119fd5b65ba952baf838bcc4fa5e
Merdjan Tekfidan <merttekfidan@gmail.com>
2020-04-25 15:25:30 +0200

    Redux setups

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
 import "./App.css";
+import store from "./store";
 
 const App = () => {
+  const { contacts } = store.getState();
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={contacts} />
       <Main />
     </div>
   );
